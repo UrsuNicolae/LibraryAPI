eb05b5c5c0f60d4239154c34253bda1f314619d6
Nicolae URSU <nicolae1.ursu@orange.com>
2024-08-23 18:59:46 +0300

    Add Library Solution with WebApi project and DataAccess project

---
+++ LibraryWebAPI/LibraryDataAccess/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace LibraryDataAccess.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+
+        public string Name { get; set; }
+
+        public ICollection<Book> Books {get;set;}
+    }
+}
