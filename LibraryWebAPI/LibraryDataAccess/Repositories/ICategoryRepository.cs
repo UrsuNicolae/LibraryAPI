dba6828ce57fed376b42225bdd85e77d55acfa6a
Nicolae URSU <nicolae1.ursu@orange.com>
2024-08-23 20:37:57 +0300

    Add CategoryRepository and tests for it

---
+++ LibraryWebAPI/LibraryDataAccess/Repositories/ICategoryRepository.cs
@@ -0,0 +1,17 @@
+﻿using LibraryDataAccess.Models;
+
+namespace LibraryDataAccess.Repositories
+{
+    public interface ICategoryRepository
+    {
+        Task<List<Category>> GetCategoriesAsync();
+
+        Task<Category?> GetCategoryByIdAsync(int id);
+
+        Task<Category> CreateCategoryAsync(Category category);
+
+        Task<Category> UpdateCategoryAsync(Category category);
+
+        Task DeleteCategoryAsync(int id);
+    }
+}
